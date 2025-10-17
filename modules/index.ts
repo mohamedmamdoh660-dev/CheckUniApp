0d5242d656cd98089f5797edd58d819bef475afa
DanishAli232 <balochdanish2020@gmail.com>
2025-10-17 18:43:52 +0500

    Remove dashboard service export from index.ts to streamline module exports and improve maintainability.

--- modules/index.ts
+++ modules/index.ts
@@ -4,7 +4,6 @@ export { rolesService } from "./roles/services/roles-service";
 export { zohoProgramsService } from "./zoho-programs/services/zoho-programs-service";
 export { zohoStudentsService } from "./zoho-students/services/zoho-students-service";
 export { zohoApplicationsService } from "./zoho-applications/services/zoho-applications-service";
-export { dashboardService } from "./dashboard/services/dashboard-service";
 
 // Export all models
 export { type User } from "@/types/types";
